b36b916577aba588885ea494484c4168bf55f0d9
Ning Xiaozhou <alt.ning1@gmail.com>
2026-06-08 13:54:50 +1000

    feat(vm): Define opcodes and chunk structure for bytecode representation

---
+++ src/opcodes.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <cstdint>
+
+enum OpCode : uint8_t {
+    OP_CONSTANT,
+    OP_NULL,
+    OP_TRUE,
+    OP_FALSE,
+
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_NEGATE,
+
+    OP_EQUAL,
+    OP_NOT_EQUAL,
+    OP_GREATER,
+    OP_GREATER_EQUAL,
+    OP_LESS,
+    OP_LESS_EQUAL,
+
+    OP_NOT,
+    OP_IN,
+
+    OP_DEFINE_GLOBAL,
+    OP_GET_GLOBAL,
+    OP_SET_GLOBAL,
+    OP_GET_LOCAL,
+    OP_SET_LOCAL,
+
+    OP_JUMP,
+    OP_JUMP_IF_FALSE,
+    OP_LOOP,
+
+    OP_CALL,
+    OP_RETURN,
+    OP_CLOSURE,
+
+    OP_ARRAY,
+    OP_DICT,
+    OP_INDEX_GET,
+    OP_INDEX_SET,
+
+    OP_GET_PROPERTY,
+    OP_SET_PROPERTY,
+    OP_NEW_INSTANCE,
+
+    OP_POP,
+    OP_DUP,
+
+    OP_FOR_PREPARE,
+    OP_FOR_LOOP,
+    OP_FOR_IN_PREPARE,
+    OP_FOR_IN_LOOP,
+    OP_AND_JUMP,
+    OP_OR_JUMP,
+    OP_CLASS,
+    OP_INHERIT,
+    OP_METHOD,
+    OP_ATTRIBUTE
+};
